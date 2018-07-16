d952b8aa4a28884953a2d31265df37251a2515d2
404pilot <404pilot@users.noreply.github.com>
2018-07-15 20:49:30 -0500

    update directories

--- book-list/src/components/app.js
+++ book-list/src/components/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-import BookList from './book-list'
-import BookDetail from './book-detail'
+import BookList from '../containers/book-list'
+import BookDetail from '../containers/book-detail'
 
 export default class App extends Component {
   render() {
