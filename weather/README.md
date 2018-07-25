5cc31d467f7776339b30d989298a86d9a7c0b8a2
404pilot <404pilot@users.noreply.github.com>
2018-07-24 23:38:55 -0500

    apply ReduxPromise middleware

---
+++ weather/README.md
@@ -0,0 +1,6 @@
+   
+Middleware is like an interceptor to manipulate data got from action before handling to reducer.
+
+```bash
+npm install --save redux-promise
+```
--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -3,7 +3,9 @@ import {FETCH_WEATHER} from "../actions/fetch_weather";
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      console.log(`Reducer -> receive action.payload: ${action.payload}`, action.payload);
+      // once ReduxPromise middleware is added, this following line will only be executed once the real data is back
+      // action.payload is not a Promise anymore, it is the real data returned from weather API server
+      console.log(`Reducer -> receive payload`, action.payload);
 
       // reducer should always return a new state (do not modify original state and return it)
       // promise.data is the real data
